0ce802aa7bfa553a95979f25f329fc28aa0a1e75
Angel <43154124+angel-utn@users.noreply.github.com>
2026-08-11 20:38:16 -0300

    Add files via upload

---
+++ Semana 01/Martes/programa1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int num1, num2, resultado;
+  cout << "Ingresar el primer numero: ";
+  cin >> num1;
+  cout << "Ingresar el segundo numero: ";
+  cin >> num1;
+  cout << endl;
+  resultado = num1 + num2;
+  cout << "El resultado de la suma es: ";
+  cout << resultado;
+  return 0;
+}
